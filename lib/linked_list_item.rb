ff6007744bc0b691247397027fc2ca87da631f32
Rachel Self <rachel.m.self@gmail.com>
2014-07-10 16:35:34 -0500

    11 tests passing

--- lib/linked_list_item.rb
+++ lib/linked_list_item.rb
@@ -1,3 +1,50 @@
 class LinkedListItem
   include Comparable
+
+  attr_reader :next_item
+  attr_reader :payload
+
+  def initialize(value)
+    @payload = value
+  end
+
+  # def next_item
+  #   @next_item
+  # end
+
+  def next_item=(other_item)
+    @next_item = other_item
+    if self == other_item
+      raise ArgumentError
+    end
+  end
+
+  def last?
+    unless @next_item
+      true
+    else
+      false
+    end
+  end
+
+  def ===(other_item)
+    self.eql?(other_item)
+  end
+
+  def <=>(other_item)
+    @payload <=> other_item.payload
+  end
+
+  # def >(item)
+  #   @payload > item.payload
+  # end
+  #
+  # def ===(item)
+  #   @payload === item.payload
+  # end
+  #
+  # def ==(item)
+  #   @payload == item.payload
+  # end
+
 end
