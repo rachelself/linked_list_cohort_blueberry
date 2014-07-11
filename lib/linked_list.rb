8c72017b3b797309d92f556a407e1e3ec9f80d8a
Rachel Self <rachel.m.self@gmail.com>
2014-07-11 15:53:25 -0500

    13 passing

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -1,13 +1,108 @@
 class LinkedList
-  # include Comparable
-  #
-  # attr_reader :next_item
-  # attr_reader :payload
-  # 
-  # def initialize(value)
-  #   @payload = value
-  # end
 
+  attr_accessor :size
 
+  def initialize(*seed)
+    @size = 0
+
+    unless seed == nil
+      seed.each do | value |
+        push(value)
+      end
+    end
+  end
+
+  def push(value)
+    @size += 1
+
+    lli = LinkedListItem.new(value)
+    if @first_item
+      last_item.next_item = lli
+    else
+      @first_item = lli
+    end
+  end
+
+  def get(index)
+    if index < 0
+      raise IndexError
+    end
+
+    current_item = @first_item
+    index.times do
+      if current_item
+        current_item = current_item.next_item
+      else
+        raise IndexError
+      end
+    end
+    current_item.payload
+  end
+
+  def last
+    unless @size == 0
+      last_item.payload
+    end
+  end
+
+  def to_s
+    if @size == 0
+      "| |"
+    else
+      payloads = []
+      count = 0
+
+      while count < @size
+        payloads << get(count)
+        count += 1
+      end
+
+      "| #{payloads.join(', ')} |"
+    end
+  end
+
+  def [](index)
+    get(index)
+  end
+
+  def []=(index, str)
+    current = replace(index, str)
+    current
+  end
+
+  def replace(index, str)
+    current_item = @first_item
+    index.times do
+      if current_item
+        current_item = current_item.next_item
+      else
+        raise IndexError
+      end
+    end
+    current_item.payload = str
+    current_item.payload
+  end
+
+  def delete(index)
+    current_item = @first_item
+    index.times do
+      if current_item
+        current_item = current_item.next_item
+        current_item
+      else
+        raise IndexError
+      end
+    end
+  end
+
+  private
+
+  def last_item
+    current_item = @first_item
+    until current_item.last?
+      current_item = current_item.next_item
+    end
+    current_item
+  end
 
 end
--- lib/linked_list_item.rb
+++ lib/linked_list_item.rb
@@ -2,49 +2,144 @@ class LinkedListItem
   include Comparable
 
   attr_reader :next_item
-  attr_reader :payload
+  attr_accessor :payload
 
   def initialize(value)
     @payload = value
   end
 
-  # def next_item
-  #   @next_item
-  # end
-
   def next_item=(other_item)
+    raise ArgumentError if other_item === self
     @next_item = other_item
-    if self == other_item
-      raise ArgumentError
-    end
   end
 
   def last?
-    unless @next_item
-      true
+    @next_item.nil?
+  end
+
+  def <=>(other_item)
+    if self.payload.class == other_item.payload.class
+      self.payload <=> other_item.payload
     else
-      false
+      precedence = [ Fixnum, String, Symbol ]
+      left = precedence.index(self.payload.class)
+      right = precedence.index(other_item.payload.class)
+      left <=> right
     end
   end
 
   def ===(other_item)
-    self.eql?(other_item)
+    self.equal? other_item
   end
-
-  def <=>(other_item)
-    @payload.to_s <=> other_item.payload.to_s
-  end
-
-  # def >(item)
-  #   @payload > item.payload
-  # end
-  #
-  # def ===(item)
-  #   @payload === item.payload
-  # end
-  #
-  # def ==(item)
-  #   @payload == item.payload
-  # end
-
 end
+
+# =======
+
+# class LinkedListItem
+#
+#   include Comparable
+#
+#   attr_reader :next_item
+#   attr_reader :payload
+#
+#   def initialize(value)
+#     @payload = value
+#   end
+#
+#   def next_item=(other_item)
+#     raise ArgumentError if other_item === self
+#     @next_item = other_item
+#   end
+#
+#   def last?
+#     @next_item.nil?
+#   end
+#
+#   def <=>(other_item)
+#     if self.payload.class == other_item.payload.class
+#       self.payload <=> other_item.payload
+#     else
+#       precedence = [ Fixnum, String, Symbol ]
+#       left = precedence.index(self.payload.class)
+#       right = precedence.index(other_item.payload.class)
+#       left <=> right
+#     end
+#   end
+#
+#   def ===(other_item)
+#     self.equal? other_item
+#   end
+# end
+
+# =======
+
+#   include Comparable
+#
+#   attr_reader :next_item
+#   attr_reader :payload
+#
+#   def initialize(value)
+#     @payload = value
+#   end
+#
+#   # def next_item
+#   #   @next_item
+#   # end
+#
+#   def next_item=(other_item)
+#     raise ArgumentError if other_item === self
+#     @next_item = other_item
+#
+#     # if self == other_item
+#     #   raise ArgumentError
+#     # end
+#   end
+#
+#   def last?
+#     @next_item.nil?
+#     # unless @next_item
+#     #   true
+#     # else
+#     #   false
+#     # end
+#   end
+#
+#   # def ===(other_item)
+#   #   self.eql?(other_item)
+#   # end
+#
+#   def <=>(other_item)
+#     if self.payload.class == other_item.payload.class
+#       self.payload <=> other_item.payload
+#     # elsif self.payload.is_a? Symbol
+#     #   1
+#     else
+#       precedence = [ Fixnum, String, Symbol ]
+#       left = precedence.index(self.payload.class)
+#       right = precedence.index(other_item.payload.class)
+#       left <=> right
+#     end
+#     # other_item.payload <=> self.payload
+#     # @payload.to_s <=> other_item.payload.to_s
+#   end
+#
+#   def ===(other_item)
+#     self.eql? other_item
+#     # self.object_id == other.item.object_id
+#   end
+#
+#
+#
+#   # def >(item)
+#   #   @payload > item.payload
+#   # end
+#   #
+#   # def ===(item)
+#   #   @payload === item.payload
+#   # end
+#   #
+#   # def ==(item)
+#   #   @payload == item.payload
+#   # end
+#
+# end
